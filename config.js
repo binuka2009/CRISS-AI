2c3a704c3c09d8c1a295d7c4a1fdfb00a86fcdd9
Binuka Lasad Udayanga <binukaudayanga3@gmail.com>
2025-12-28 20:57:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,83 +1,46 @@
-
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CRISS-AI~TtAnGBpD#ezWxm4p26xWvX1huS5yVME_GxKDvk5pwqEfdzEwqpgQ",
-// add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-// make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-// make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
-// make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY CRISS-AI 🤍*",
-// set the auto reply massage on status reply  
-WELCOME: process.env.WELCOME || "false",
-// true if want welcome and goodbye msg in groups    
-ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-// make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "true",
-// make anti link true,false for groups 
-MENTION_REPLY: process.env.MENTION_REPLY || "false",
-// make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://res.cloudinary.com/dgy2dutjs/image/upload/v1751624587/url.crissvevo.co.tz/IMG_2353_fze42l.jpg",
-// add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
-// add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "CRISS-AI",
-// add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "CRISS-AI",
-// type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-// make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
-// chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
-// automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94724659430",
-// add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "ᴄʀɪss ᴠᴇᴠᴏ",
-// add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀɪss ᴠᴇᴠᴏ*",
-// add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://res.cloudinary.com/dgy2dutjs/image/upload/v1751624587/url.crissvevo.co.tz/IMG_2353_fze42l.jpg",
-// add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *CRISS-AI*⚡",
-// add alive msg here 
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
-// Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
-// make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
-// false or true for anti bad words  
-MODE: process.env.MODE || "private",
-// make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
-// make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
-// make true for send automatic voices
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-// make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-// make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-// maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
-// make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
-// true for automatic show typing   
-READ_CMD: process.env.READ_CMD || "false",
-// true if want mark commands as read 
-DEV: process.env.DEV || "94724659430",
-//replace with your whatsapp number        
-ANTI_VV: process.env.ANTI_VV || "true",
-// true for anti once view 
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
-// change it to 'same' if you want to resend deleted message in same chat 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
-// make it true for auto recoding 
+    SESSION_ID: process.env.SESSION_ID || "CRISS-AI~TtAnGBpD#ezWxm4p26xWvX1huS5yVME_GxKDvk5pwqEfdzEwqpgQ",
+    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY CRISS-AI 🤍*",
+    WELCOME: process.env.WELCOME || "false",
+    ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+    ANTI_LINK: process.env.ANTI_LINK || "true",
+    MENTION_REPLY: process.env.MENTION_REPLY || "false",
+    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://res.cloudinary.com/dgy2dutjs/image/upload/v1751624587/url.crissvevo.co.tz/IMG_2353_fze42l.jpg",
+    PREFIX: process.env.PREFIX || ".",
+    BOT_NAME: process.env.BOT_NAME || "CRISS-AI",
+    STICKER_NAME: process.env.STICKER_NAME || "CRISS-AI",
+    CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+    CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+    DELETE_LINKS: process.env.DELETE_LINKS || "false",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "94724659430",
+    OWNER_NAME: process.env.OWNER_NAME || "ᴄʀɪss ᴠᴇᴠᴏ",
+    DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀɪss ᴠᴇᴠᴏ*",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://res.cloudinary.com/dgy2dutjs/image/upload/v1751624587/url.crissvevo.co.tz/IMG_2353_fze42l.jpg",
+    LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *CRISS-AI*⚡",
+    READ_MESSAGE: process.env.READ_MESSAGE || "false",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    ANTI_BAD: process.env.ANTI_BAD || "false",
+    MODE: process.env.MODE || "private", // පොදු භාවිතයට 'public' කරන්න
+    ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+    AUTO_VOICE: process.env.AUTO_VOICE || "false",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+    PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
+    AUTO_TYPING: process.env.AUTO_TYPING || "false",
+    READ_CMD: process.env.READ_CMD || "false",
+    DEV: process.env.DEV || "94724659430",
+    ANTI_VV: process.env.ANTI_VV || "true",
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
+    AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 };
